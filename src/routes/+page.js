d506b7368988d6bc908b0b046e8b8236f2bf53f5
bharath <broxdeez@gmail.com>
2023-05-26 19:01:28 -0700

    init

---
+++ src/routes/+page.js
@@ -0,0 +1,17 @@
+export const load = async ({ fetch }) => {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+    const json = await response.json()
+    const monsters = json.results.map(monster => {
+        const splitUrl = monster.url.split('/')
+        const id = splitUrl[splitUrl.length - 2]
+        return {
+            name: monster.name,
+            url: monster.url,
+            id
+        }
+    })
+
+    return {
+        monsters
+    }
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { generations } from "./generations";
+    export let data;
+</script>
+
+{#each data.monsters as monster (monster.id)}
+    <p>{monster.id} : {monster.name}</p>
+{/each}
+
+{#each generations as generation}
+    <p>{generation.name}</p>
+    <p>Main Region : {generation.main_region}</p>
+    <p>Games : {generation.games.join(", ")}</p>
+{/each}
+
+<style>
+</style>
